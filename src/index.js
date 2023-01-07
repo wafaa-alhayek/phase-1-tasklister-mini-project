5a70af71f4b02c6e85606f338e3f106a1bc11f95
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-07 16:55:02 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e.target.new-task-description.value)
+    form.reset()
+  })
 });
+function handleToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
